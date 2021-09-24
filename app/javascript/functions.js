b90a59cb0091f68ffb815cd1d27f088670e08875
grum1n <grumin.el.pastas@gmail.com>
2021-09-24 15:29:05 +0300

    deleted examples section

--- app/javascript/functions.js
+++ app/javascript/functions.js
@@ -27,19 +27,3 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-/* START OF ACCORDION */
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
-/* END OF ACCORDION */
